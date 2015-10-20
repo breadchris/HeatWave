a9752e714375d375d24c022dc6dc779d64df18cc
Josh Hofing <joshhofing@gmail.com>
2015-10-19 22:00:15 -0400

    Random C generation ish kinda wtf

--- random_c.py
+++ random_c.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
+import sys
+sys.setrecursionlimit(2**16)
 c_types = ['char', 'short', 'int', 'long']
+
 
 class Node(object):
     possible_children = []
     """ Not actually instantiable, but used as a base so possible children can match on it for "anything goes" """
     pass
 
+class TypeName(Node):
+    possible_children = [(str,)]
+    def __init__(self, typ):
+        self.typ = random.choice(c_types)
+    def dump(self):
+        return str(self.typ)
+
 class Literal(Node):
-    possible_children = [(str, int)
+    possible_children = [(int,)
                         ]
     def __init__(self, value):
         self.value = value
@@ -15,7 +25,7 @@ class Literal(Node):
         return repr(self.value).replace("'", '"')
 
 class Variable(Node):
-    possible_children = [(str)
+    possible_children = [(str,)
                         ]
     def __init__(self, name):
         self.name = name
@@ -23,39 +33,81 @@ class Variable(Node):
         return self.name
 
 class Decl(Node):
-    possible_children = [(Variable),
-                         (Variable, Literal, None)
+    possible_children = [(TypeName,),
+                         (Variable,),
+                         (Literal, None)
                         ]
     def __init__(self, typ, name, value=None):
         self.typ = typ
         self.name = name
         self.value = value
     def dump(self):
-        return self.typ + ' ' + self.name.dump() + (' = ' + self.value.dump() if self.value else '')
+        return self.typ.dump() + ' ' + self.name.dump() + (' = ' + self.value.dump() if self.value else '')
 
 class Block(Node):
-    possible_children = [(Node),
+    possible_children = [(...,),
                          ...
                         ]
-    def __init__(self, elements=None):
+    def __init__(self, *elements):
         self.elements = elements if elements else []
     def dump(self):
         return '{\n' + ';\n'.join([x.dump() for x in self.elements]) + ';\n}\n'
 
 class IfStmt(Node):
-    possible_children = [(Literal, Variable),
-                         (Node),
-                         (Node, None)
+    possible_children = [(Literal,),
+                         (Block, Literal,),
                         ]
     def __init__(self, condition, true_part, false_part=None):
         self.condition = condition
         self.true_part = true_part
         self.false_part = false_part
     def dump(self):
-        return "if (" + self.condition.dump() + ")\n" + self.true_part.dump() + (' else ' + self.false_part.dump() if self.false_part else '') 
+        return "if (rand())\n" + self.true_part.dump() + ('; else ' + self.false_part.dump() + '' if self.false_part else ';') 
+
+import random
+
+def generate_thing():
+    kinds = Node.__subclasses__()
+    node_cl = random.choice(kinds)
+    print(node_cl)
+    print(node_cl.possible_children)
+
+def generate_children_for(node_cl, remaining_depth=3):
+    if remaining_depth == 0:
+        return Variable("printf(\"lolhitrecursionlimit\\n\")")
+    possible = node_cl.possible_children
+    variadic = possible[-1] == ...
+    choices = []
+    if variadic:
+        if possible[0][0] == ...:
+            choices = [random.choice([IfStmt, Literal])]
+            ext_choices = list(map(random.choice, [[IfStmt, Literal]] * random.randint(0, 100)))
+        else:
+            choices = list(map(random.choice, possible[:-1]))
+            ext_choices = list(map(random.choice, [possible[-2]] * random.randint(0, 100)))
+        choices += ext_choices
+    else:
+        choices = list(map(random.choice, possible))
+    def initialize_node(node):
+        if node:
+            if issubclass(node, Node):
+                return generate_children_for(node, remaining_depth-1)
+            else:
+                if node is str:
+                    return ''.join(map(random.choice, ["abcdefghijklmonp"]*3))
+                elif node is int:
+                    return 1
+                else:
+                    print("WTF %r isn't a type I know about!?!?"%node)
+        else:
+            return None
+    children = list(map(initialize_node, choices))
+    return node_cl(*children)
+    
 
 
 if __name__ == '__main__':
-    prologue = "int main() {"
+    prologue = "#include <stdio.h>\nint main() {"
     epilogue = "return 0;}"
-    print(prologue + Block([Decl('int', Variable('x'), Literal(1)), Literal("234"), IfStmt(Literal(1), Block([Literal("asdf"), Literal("fsad")]), Literal('1'))]).dump() + epilogue)
+    print(prologue + generate_children_for(Block).dump() + epilogue)
+    #print(prologue + Block([Decl('int', Variable('x'), Literal(1)), Literal("234"), IfStmt(Literal(1), Block([Literal("asdf"), Literal("fsad")]), Literal('1'))]).dump() + epilogue)
